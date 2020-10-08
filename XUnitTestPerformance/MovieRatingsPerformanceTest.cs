f973f0b8c0f0a119cda13228cb1b4497fa777bb2
Andreas Villumsen <andreasvillumsen@MacBook-Pro.localdomain>
2020-10-08 23:23:20 +0200

    Test Performance Tests - Some static data

--- XUnitTestPerformance/MovieRatingsPerformanceTest.cs
+++ XUnitTestPerformance/MovieRatingsPerformanceTest.cs
@@ -12,24 +12,40 @@ namespace XUnitTestPerformance
         private readonly ITestOutputHelper _testOutputHelper;
         private IMovieRatingsRepository repository;
 
-        private int reviewerMostReviews;
-        private int movieMostReviews;
+        private const int _timeLimit = 4;
+
+        private int _reviewerMostReviews;
+        private int _movieMostReviews;
 
 
         public MovieRatingsPerformanceTest(TestFixture data, ITestOutputHelper testOutputHelper)
         {
             _testOutputHelper = testOutputHelper;
             repository = data.Repository;
-            reviewerMostReviews = data.ReviewerMostReviews;
-            movieMostReviews = data.MovieMostReviews;
+            _reviewerMostReviews = data.ReviewerMostReviews;
+            _movieMostReviews = data.MovieMostReviews;
         }
 
         private double TimeInSeconds(Action ac)
         {
-            Stopwatch sw = Stopwatch.StartNew();
+            var sw = Stopwatch.StartNew();
             ac.Invoke();
             sw.Stop();
             return sw.ElapsedMilliseconds / 1000d;
+        }
+        
+        [Fact]
+        public void NumberOfMoviesWithGrade()
+        {
+            MovieRatingsService mrs = new MovieRatingsService(repository);
+
+            double seconds = TimeInSeconds(() =>
+            {
+                var result = mrs.NumberOfMoviesWithGrade(5);
+            });
+
+            _testOutputHelper.WriteLine(seconds.ToString());
+            Assert.True(seconds <= _timeLimit);
         }
 
         [Fact]
@@ -39,11 +55,151 @@ namespace XUnitTestPerformance
 
             double seconds = TimeInSeconds(() =>
             {
-                int result = mrs.GetNumberOfReviewsFromReviewer(reviewerMostReviews);
+                var result = mrs.GetNumberOfReviewsFromReviewer(_reviewerMostReviews);
             });
 
             _testOutputHelper.WriteLine(seconds.ToString());
-            Assert.True(seconds <= 4);
+            Assert.True(seconds <= _timeLimit);
+        }
+        
+        [Fact]
+        public void GetAverageRateFromReviewer()
+        {
+            MovieRatingsService mrs = new MovieRatingsService(repository);
+
+            double seconds = TimeInSeconds(() =>
+            {
+                var result = mrs.GetAverageRateFromReviewer(_reviewerMostReviews);
+            });
+
+            _testOutputHelper.WriteLine(seconds.ToString());
+            Assert.True(seconds <= _timeLimit);
+        }
+        
+        [Fact]
+        public void GetNumberOfRatesByReviewer()
+        {
+            MovieRatingsService mrs = new MovieRatingsService(repository);
+
+            double seconds = TimeInSeconds(() =>
+            {
+                var result = mrs.GetNumberOfRatesByReviewer(_reviewerMostReviews, 3);
+            });
+
+            _testOutputHelper.WriteLine(seconds.ToString());
+            Assert.True(seconds <= _timeLimit);
+        }
+        
+        [Fact]
+        public void GetNumberOfReviews()
+        {
+            MovieRatingsService mrs = new MovieRatingsService(repository);
+
+            double seconds = TimeInSeconds(() =>
+            {
+                var result = mrs.GetNumberOfReviews(_movieMostReviews);
+            });
+
+            _testOutputHelper.WriteLine(seconds.ToString());
+            Assert.True(seconds <= _timeLimit);
+        }
+        
+        [Fact]
+        public void GetAverageRateOfMovie()
+        {
+            MovieRatingsService mrs = new MovieRatingsService(repository);
+
+            double seconds = TimeInSeconds(() =>
+            {
+                var result = mrs.GetAverageRateOfMovie(_movieMostReviews);
+            });
+
+            _testOutputHelper.WriteLine(seconds.ToString());
+            Assert.True(seconds <= _timeLimit);
+        }
+        
+        [Fact]
+        public void GetNumberOfRates()
+        {
+            MovieRatingsService mrs = new MovieRatingsService(repository);
+
+            double seconds = TimeInSeconds(() =>
+            {
+                var result = mrs.GetNumberOfRates(_movieMostReviews, 2);
+            });
+
+            _testOutputHelper.WriteLine(seconds.ToString());
+            Assert.True(seconds <= _timeLimit);
+        }
+        
+        [Fact]
+        public void GetMoviesWithHighestNumberOfTopRates()
+        {
+            MovieRatingsService mrs = new MovieRatingsService(repository);
+
+            double seconds = TimeInSeconds(() =>
+            {
+                var result = mrs.GetMoviesWithHighestNumberOfTopRates();
+            });
+
+            _testOutputHelper.WriteLine(seconds.ToString());
+            Assert.True(seconds <= _timeLimit);
+        }
+        
+        [Fact]
+        public void GetMostProductiveReviewers()
+        {
+            MovieRatingsService mrs = new MovieRatingsService(repository);
+
+            double seconds = TimeInSeconds(() =>
+            {
+                var result = mrs.GetMostProductiveReviewers();
+            });
+
+            _testOutputHelper.WriteLine(seconds.ToString());
+            Assert.True(seconds <= _timeLimit);
+        }
+        
+        [Fact]
+        public void GetTopRatedMovies()
+        {
+            MovieRatingsService mrs = new MovieRatingsService(repository);
+
+            double seconds = TimeInSeconds(() =>
+            {
+                var result = mrs.GetTopRatedMovies(2);
+            });
+
+            _testOutputHelper.WriteLine(seconds.ToString());
+            Assert.True(seconds <= 5); // Test is just above 4. Result: 4.321 seconds
+        }
+        
+        [Fact]
+        public void GetTopMoviesByReviewer()
+        {
+            MovieRatingsService mrs = new MovieRatingsService(repository);
+
+            double seconds = TimeInSeconds(() =>
+            {
+                var result = mrs.GetTopMoviesByReviewer(_reviewerMostReviews);
+            });
+
+            _testOutputHelper.WriteLine(seconds.ToString());
+            Assert.True(seconds <= _timeLimit);
+        }
+        
+        [Fact]
+        public void GetReviewersByMovie()
+        {
+            MovieRatingsService mrs = new MovieRatingsService(repository);
+
+            double seconds = TimeInSeconds(() =>
+            {
+                var result = mrs.GetReviewersByMovie(_movieMostReviews);
+            });
+
+            _testOutputHelper.WriteLine(seconds.ToString());
+            Assert.True(seconds <= _timeLimit);
         }
     }
 }
